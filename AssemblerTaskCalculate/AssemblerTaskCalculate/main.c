e3f8a9f73d070d5acae67e17096184612082c099
mctavish141 <Serega@MacBook-Air.local>
2015-10-01 18:57:20 +0300

    Initial commit

---
+++ AssemblerTaskCalculate/AssemblerTaskCalculate/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AssemblerTaskCalculate
+//
+//  Created by Serhii Kopach on 01.10.15.
+//  Copyright (c) 2015 Serhii Kopach. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
