ee8af3a1df636388d456e9aa26191ce45076554f
mctavish141 <Serega@MacBook-Air.local>
2015-10-01 11:04:23 +0300

    Initial commit

---
+++ AssemblerTask/AssemblerTask/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AssemblerTask
+//
+//  Created by Serhii Kopach on 01.10.15.
+//  Copyright (c) 2015 Serhii Kopach. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
