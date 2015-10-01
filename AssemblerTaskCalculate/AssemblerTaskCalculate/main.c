9779471d4697aa8df10849507fe7888a41cb4e7d
mctavish141 <Serega@MacBook-Air.local>
2015-10-01 18:58:33 +0300

    Version 1.0

--- AssemblerTaskCalculate/AssemblerTaskCalculate/main.c
+++ AssemblerTaskCalculate/AssemblerTaskCalculate/main.c
@@ -8,8 +8,45 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void sum () {
+    __asm__(
+            "add %ebx,%eax\n"
+            );
+}
+void substract () {
+    __asm__(
+            "sub %ebx,%eax\n"
+            );
+}
+void multiply () {
+    __asm__(
+            "imul %ebx,%eax\n"
+            );
+}
+void divide () {
+    __asm__(
+            "cdq\n"
+            "idivl %ebx\n"
+            );
+}
+
+int main () {
+    int a = 12;
+    int b = 4;
+    int result;
+    
+    __asm__(
+            "mov %0,%%eax\n"
+            "mov %1,%%ebx\n"
+            "call _multiply\n"
+            "mov %%eax,%%ecx\n"
+            "mov %0,%%eax\n"
+            "mov %1,%%ebx\n"
+            "call _divide\n"
+            "sub %%eax,%%ecx\n"
+            "mov %%ecx,%2\n"
+            : "+m" (a), "+m" (b), "+m" (result));
+    
+    printf("Result: %d\n", result);
     return 0;
 }
