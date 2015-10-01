b766e3817afba7a89c156ea803b0bd31eea979ca
mctavish141 <Serega@MacBook-Air.local>
2015-10-01 11:35:11 +0300

    Multiply and divide functions added

--- AssemblerTask/AssemblerTask/main.cpp
+++ AssemblerTask/AssemblerTask/main.cpp
@@ -6,10 +6,41 @@
 //  Copyright (c) 2015 Serhii Kopach. All rights reserved.
 //
 
-#include <iostream>
+#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int multiply (int a, int b) {
+    int result;
+    
+    __asm__(
+            "mov %0,%%eax\n"
+            "mov %1,%%ecx\n"
+            "imul %%eax,%%ecx\n"
+            "mov %%ecx,%2\n"
+            : "+r" (a), "+r" (b), "+m" (result));
+    
+    return result;
+}
+
+int divide (int a, int b) {
+    int result;
+    
+    __asm__(
+            "mov %0,%%eax\n"
+            "cdq\n"
+            "mov %1,%%ebx\n"
+            "idivl %%ebx\n"
+            "mov %%eax,%2\n"
+            : "+r" (a), "+r" (b), "+m" (result));
+    
+    return result;
+}
+
+int main() {
+    int a = 21;
+    int b = 3;
+    int result = divide(a, b);
+    
+    printf("Result: %d\n", result);
+    
     return 0;
 }
